9970bc7c4dec2f31cfbd3d54eeb313cb21a314a7
feiqingjing <qingjing_02@126.com>
2020-03-15 22:07:15 +0800

    creat common package.

---
+++ offer_test/src/common/ListNode.java
@@ -0,0 +1,18 @@
+package common;
+
+public class ListNode {
+  public int val;
+  public ListNode next;
+
+  public ListNode(){}
+  public ListNode(int value){ this.val = value; }
+  public ListNode(int value, ListNode nextNode){
+    this.val = value;
+    this.next = nextNode;
+  }
+
+  public int getVal(){ return this.val; }
+  public ListNode nextNode(){ return this.next; }
+  public ListNode next() { return this.next; }
+  public void setNext(ListNode node){ this.next = node; }
+}
