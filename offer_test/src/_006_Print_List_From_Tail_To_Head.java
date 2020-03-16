c1be716c42a00705acb5c8b6bc4df0a018f46b96
feiqingjing <qingjing_02@126.com>
2020-03-16 10:03:13 +0800

    resubmit 006 with 4 solutions.

--- offer_test/src/_006_Print_List_From_Tail_To_Head.java
+++ offer_test/src/_006_Print_List_From_Tail_To_Head.java
@@ -1,23 +1,26 @@
 import java.util.ArrayList;
 import common.ListNode;
 import java.util.LinkedList;
-import sun.awt.image.ImageWatched.Link;
 
 public class _006_Print_List_From_Tail_To_Head {
 
   private static class Solution {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
       ListNode nextNode = new ListNode(1);
       ListNode headNode = new ListNode(0, nextNode);
-      for(int i = 2; i < 10; i++){
+      for (int i = 2; i < 10; i++) {
         ListNode node = new ListNode(i);
         nextNode.setNext(node);
         nextNode = node;
       }
-      ArrayList<Integer> res = printListFromTailToHead_1(headNode);
+      ArrayList<Integer> res = printListFromTailToHead_4(headNode);
+      System.out.println(res);
     }
 
+    /*
+    1. 反转链表指针，改变了原链表
+     */
     public static ArrayList<Integer> printListFromTailToHead_1(ListNode listNode) {
       ListNode pre = null;
       while (listNode != null) {
@@ -34,6 +37,9 @@ public class _006_Print_List_From_Tail_To_Head {
       return list;
     }
 
+    /*
+    2. 利用ArrayList.add(0, E)，在表头插入
+     */
     public static ArrayList<Integer> printListFromTailToHead_2(ListNode listNode) {
       ArrayList<Integer> list = new ArrayList<>();
       ListNode node = listNode;
@@ -44,15 +50,32 @@ public class _006_Print_List_From_Tail_To_Head {
       return list;
     }
 
+    /*
+    3. 栈，不改变原链表
+     */
     public static ArrayList<Integer> printListFromTailToHead_3(ListNode listNode) {
-      LinkedList<Integer> stack = new LinkedList<>();
       ArrayList<Integer> list = new ArrayList<>();
+      LinkedList<Integer> stack = new LinkedList<>();
       while (listNode != null) {
         stack.push(listNode.val);
         listNode = listNode.next;
       }
       while (!stack.isEmpty()) {
         list.add(stack.pop());
+      }
+      return list;
+    }
+
+    /*
+    4. 递归，可以用栈就可以用递归；
+       但若链表非常长，会导致函数调用层级很深，从而导致函数调用栈溢出，用栈更安全。
+     */
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    public static ArrayList<Integer> printListFromTailToHead_4(ListNode listNode) {
+      if (listNode != null) {
+        printListFromTailToHead_4(listNode.next);
+        list.add(listNode.val);
       }
       return list;
     }
