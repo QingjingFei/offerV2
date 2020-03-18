53b99b5177a922f819dffe1fef4abf8d45b5b299
feiqingjing <qingjing_02@126.com>
2020-03-18 09:58:50 +0800

    resubmit 003_1.

--- offer_test/src/_003_1_Repeat_Num_In_Array.java
+++ offer_test/src/_003_1_Repeat_Num_In_Array.java
@@ -19,7 +19,12 @@ public class _003_1_Repeat_Num_In_Array {
         System.out.println("result = " + result + ", duplication = " + duplication[0]);
       }
     }
-
+    /*
+    若array[i]与下标i不相等，则把array[i]与array[array[i]交换，把array[i]换到它应在的位置去；
+    duplication[]仅存储一个重复数字，因为题目也并未要求打印出所有重复数组；
+    注意代码开头异常特殊测试用例的处理。
+    代码中尽管有一个两重循环，但每个数字最多只要交换两次就能找到属于它自己的位置，因此时间复杂度是O(n)
+     */
     public boolean duplicate(int[] array, int[] duplication) {
       int n = array.length;
       if (array == null || n == 1) {
@@ -36,7 +41,7 @@ public class _003_1_Repeat_Num_In_Array {
             duplication[0] = array[i];
             return true;
           }
-
+          // 交换array[i]和array[array[i]]
           int tmp = array[i];
           array[i] = array[tmp];
           array[tmp] = tmp;
