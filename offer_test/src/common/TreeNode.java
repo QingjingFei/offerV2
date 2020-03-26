008501c59d4af3e1e96c2281d0c46e3969da5479
feiqingjing <qingjing_02@126.com>
2020-03-26 10:28:01 +0800

    add print() method to print TreeNode in lever order.

--- offer_test/src/common/TreeNode.java
+++ offer_test/src/common/TreeNode.java
@@ -1,5 +1,9 @@
 package common;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
 
   public int val = 0;
@@ -8,6 +12,25 @@ public class TreeNode {
 
   public TreeNode(int value) {
     this.val = value;
+  }
+
+  public ArrayList<ArrayList<Integer>> print() {
+    ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+    if (this == null) return res;
+    Queue<TreeNode> q = new LinkedList<>();
+    q.offer(this);
+    while (!q.isEmpty()) {
+      int size = q.size();
+      ArrayList<Integer> list = new ArrayList<>();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = q.poll();
+        list.add(node.val);
+        if (node.left != null) q.offer(node.left);
+        if (node.right != null) q.offer(node.right);
+      }
+      res.add(list);
+    }
+    return res;
   }
 
 }
